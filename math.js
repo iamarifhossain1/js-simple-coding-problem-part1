2b2a573e432a6dc8007530b3f628739f9b48df80
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-12 12:16:31 +0600

    math

---
+++ math.js
@@ -0,0 +1,12 @@
+const min = Math.min(2, 11, 45, 76, 35, 9, 5, 7, 165, 205, 999);
+console.log('Min Number:', min);
+
+const max = Math.max(2, 11, 45, 76, 35, 9, 5, 7, 165, 205, 999);
+console.log('Max Number:', max);
+
+console.log("Round Number:", Math.round(2.75));
+console.log("Round Number:", Math.round(4.12));
+console.log("Round Number:", Math.floor(10.99));
+console.log("Round Number:", Math.ceil(10.19));
+const rand = Math.round(Math.random() * 100);
+console.log(rand);
